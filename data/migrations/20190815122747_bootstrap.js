6fa3e11eb74db26e948e13a8e47f7c942758a089
Reed James <reedtjames1234@gmail.com>
2019-08-15 17:11:07 -0500

    Finished lecture

---
+++ data/migrations/20190815122747_bootstrap.js
@@ -0,0 +1,64 @@
+exports.up = function(knex) {
+  return knex.schema
+    .createTable("zoos", tbl => {
+      tbl.increments();
+
+      tbl
+        .string("zoo_name", 255)
+        .notNullable()
+        .unique();
+      tbl.string("address", 4000);
+    })
+    .createTable("species", tbl => {
+      tbl.increments();
+
+      tbl
+        .string("species_name", 255)
+        .notNullable()
+        .unique();
+    })
+    .createTable("animals", tbl => {
+      tbl.increments();
+
+      tbl.string("animal_name", 255).notNullable();
+
+      //a forieng key!
+      tbl
+        .integer("species_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("species")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE"); //if the pk on the primary table changes, what should it do
+
+      //CASCADE, RESTRICT,  NO ACTION, SET DEFAULT,
+    })
+    .createTable("zoo_animals", tbl => {
+      tbl.increments();
+      tbl
+        .integer("zoo_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("zoos")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE");
+      tbl
+        .integer("animal_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("animals")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE");
+    });
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTableIfExists("zoo_animals")
+    .dropTableIfExists("animals")
+    .dropTableIfExists("species")
+    .dropTableIfExists("zoos");
+};
